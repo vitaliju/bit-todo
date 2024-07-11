55f67bb08eb4cafac0553e4f9275b5149b271fdd
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-07-11 15:23:32 +0300

    article file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-// rikiavimas?..
-// localtorage
+// rikiavimas?.. 😏
+// localStorage
 
 const h1DOM = document.querySelector('h1');
 const formDOM = document.forms[0];
@@ -7,21 +7,101 @@ const textInputDOM = formDOM.querySelector('input');
 const submitButtonDOM = formDOM.querySelector('button');
 const listDOM = document.querySelector('.list');
 
+const todoData = [];
+
 submitButtonDOM.addEventListener('click', e => {
     e.preventDefault();
 
-    console.log(textInputDOM.value);
+    if (textInputDOM.value.length === 0) {
+        return;
+    }
 
-    const HTML = `
-        <article class="items">
-            <div class="text">${textInputDOM.value}</div>
-            <div class="actions">
-                <button>Done</button>
-                <div class="divider"></div>
-                <button>Edit</button>
-                <button>Delete</button>
-            </div>
-        </article>`;
-    
-    listDOM.innerHTML += HTML;
+    todoData.push({
+        text: textInputDOM.value,
+        createdAt: Date.now(),
+    });
+    renderList();
 });
+
+function renderList() {
+    if (todoData.length === 0) {
+        renderEmptyList();
+    } else {
+        renderTaskList();
+    }
+}
+
+function renderEmptyList() {
+    listDOM.classList.add('empty');
+    listDOM.textContent = 'Empty';
+}
+
+function renderTaskList() {
+    let HTML = '';
+
+    for (const todo of todoData) {
+        HTML += `
+            <article class="item">
+                <div class="date">${formatTime(todo.createdAt)}</div>
+                <div class="text">${todo.text}</div>
+                <form class="hidden">
+                    <input type="text">
+                    <button type="submit">Update</button>
+                    <button type="button">Cancel</button>
+                </form>
+                <div class="actions">
+                    <button>Done</button>
+                    <div class="divider"></div>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+            </article>`;
+    }
+
+    listDOM.innerHTML = HTML;
+    listDOM.classList.remove('empty');
+
+    const articlesDOM = listDOM.querySelectorAll('article');
+
+    for (let i = 0; i < articlesDOM.length; i++) {
+        const articleDOM = articlesDOM[i];
+        const articleEditFormDOM = articleDOM.querySelector('form');
+        const updateInputDOM = articleEditFormDOM.querySelector('input');
+        const buttonsDOM = articleDOM.querySelectorAll('button');
+
+        const updateDOM = buttonsDOM[0];
+        updateDOM.addEventListener('click', event => {
+            event.preventDefault();
+            todoData[i] = updateInputDOM.value;
+            renderTaskList();
+        });
+
+        const cancelDOM = buttonsDOM[1];
+        cancelDOM.addEventListener('click', () => {
+            articleEditFormDOM.classList.add('hidden');
+        });
+
+        const editDOM = buttonsDOM[3];
+        editDOM.addEventListener('click', () => {
+            articleEditFormDOM.classList.remove('hidden');
+        });
+
+        const deleteDOM = buttonsDOM[4];
+        deleteDOM.addEventListener('click', () => {
+            todoData.splice(i, 1);
+            renderList();
+        });
+    }
+}
+
+function formatTime(timeInMs) {
+    // mdn: js Date -> getYear, getMonth, getDay, getHour...
+    return '2024-07-11 14:36:17';
+}
+
+// CRUD operations:
+// -----------------------------------
+// create   array.push({initial data})
+// read     array.map()
+// update   array[i] = {updated data}
+// delete   array.splice(i, 1)s
