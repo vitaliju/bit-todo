0c428ba1d8e00cadc2bf89d9bd22f52b4816b063
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-07-15 15:21:04 +0300

    format date

--- js/main.js
+++ js/main.js
@@ -1,46 +1,46 @@
 // rikiavimas?.. 😏
 // localStorage
 
-const h1DOM = document.querySelector('h1');
+const h1DOM = document.querySelector("h1");
 const formDOM = document.forms[0];
-const textInputDOM = formDOM.querySelector('input');
-const submitButtonDOM = formDOM.querySelector('button');
-const listDOM = document.querySelector('.list');
+const textInputDOM = formDOM.querySelector("input");
+const submitButtonDOM = formDOM.querySelector("button");
+const listDOM = document.querySelector(".list");
 
 const todoData = [];
 
-submitButtonDOM.addEventListener('click', e => {
-    e.preventDefault();
+submitButtonDOM.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    if (textInputDOM.value.length === 0) {
-        return;
-    }
+  if (!isValidText(textInputDOM.value)) {
+    return;
+  }
 
-    todoData.push({
-        text: textInputDOM.value,
-        createdAt: Date.now(),
-    });
-    renderList();
+  todoData.push({
+    text: textInputDOM.value.trim(),
+    createdAt: Date.now(),
+  });
+  renderList();
 });
 
 function renderList() {
-    if (todoData.length === 0) {
-        renderEmptyList();
-    } else {
-        renderTaskList();
-    }
+  if (todoData.length === 0) {
+    renderEmptyList();
+  } else {
+    renderTaskList();
+  }
 }
 
 function renderEmptyList() {
-    listDOM.classList.add('empty');
-    listDOM.textContent = 'Empty';
+  listDOM.classList.add("empty");
+  listDOM.textContent = "Empty";
 }
 
 function renderTaskList() {
-    let HTML = '';
+  let HTML = "";
 
-    for (const todo of todoData) {
-        HTML += `
+  for (const todo of todoData) {
+    HTML += `
             <article class="item">
                 <div class="date">${formatTime(todo.createdAt)}</div>
                 <div class="text">${todo.text}</div>
@@ -56,47 +56,106 @@ function renderTaskList() {
                     <button>Delete</button>
                 </div>
             </article>`;
-    }
+  }
 
-    listDOM.innerHTML = HTML;
-    listDOM.classList.remove('empty');
+  listDOM.innerHTML = HTML;
+  listDOM.classList.remove("empty");
 
-    const articlesDOM = listDOM.querySelectorAll('article');
+  const articlesDOM = listDOM.querySelectorAll("article");
 
-    for (let i = 0; i < articlesDOM.length; i++) {
-        const articleDOM = articlesDOM[i];
-        const articleEditFormDOM = articleDOM.querySelector('form');
-        const updateInputDOM = articleEditFormDOM.querySelector('input');
-        const buttonsDOM = articleDOM.querySelectorAll('button');
+  for (let i = 0; i < articlesDOM.length; i++) {
+    const articleDOM = articlesDOM[i];
+    const articleEditFormDOM = articleDOM.querySelector("form");
+    const updateInputDOM = articleEditFormDOM.querySelector("input");
+    const buttonsDOM = articleDOM.querySelectorAll("button");
 
-        const updateDOM = buttonsDOM[0];
-        updateDOM.addEventListener('click', event => {
-            event.preventDefault();
-            todoData[i] = updateInputDOM.value;
-            renderTaskList();
-        });
+    const updateDOM = buttonsDOM[0];
+    updateDOM.addEventListener("click", (event) => {
+      event.preventDefault();
 
-        const cancelDOM = buttonsDOM[1];
-        cancelDOM.addEventListener('click', () => {
-            articleEditFormDOM.classList.add('hidden');
-        });
+      if (!isValidText(updateInputDOM.value)) {
+        return;
+      }
+      todoData[i].text = updateInputDOM.value.trim();
+      renderTaskList();
+    });
 
-        const editDOM = buttonsDOM[3];
-        editDOM.addEventListener('click', () => {
-            articleEditFormDOM.classList.remove('hidden');
-        });
+    const cancelDOM = buttonsDOM[1];
+    cancelDOM.addEventListener("click", () => {
+      articleEditFormDOM.classList.add("hidden");
+    });
 
-        const deleteDOM = buttonsDOM[4];
-        deleteDOM.addEventListener('click', () => {
-            todoData.splice(i, 1);
-            renderList();
-        });
-    }
+    const editDOM = buttonsDOM[3];
+    editDOM.addEventListener("click", () => {
+      articleEditFormDOM.classList.remove("hidden");
+    });
+
+    const deleteDOM = buttonsDOM[4];
+    deleteDOM.addEventListener("click", () => {
+      todoData.splice(i, 1);
+      renderList();
+    });
+  }
 }
 
 function formatTime(timeInMs) {
-    // mdn: js Date -> getYear, getMonth, getDay, getHour...
-    return '2024-07-11 14:36:17';
+  // minimum 100-ieji metai
+  // maximum ???
+  const date = new Date();
+  const y = date.getFullYear();
+  const m = (date.getMonth() < 9 ? "0" : "") + (date.getMonth() + 1);
+  const d = (date.getDay() < 10 ? "0" : "") + date.getDay();
+  const h = date.getHours();
+  const mn = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+  const s = (date.getSeconds() < 10 ? "0" : "") + date.getSeconds();
+  return `${y}-${m}-${d} ${h}:${mn}:${s}`;
+}
+
+/* function formatTime() {
+     // mdn: js Date -> getYear, getMonth, getDay, getHour...
+       
+     let now = new Date(); 
+     let year    = now.getFullYear();
+     let month   = now.getMonth() + 1; 
+     let day     = now.getDate();
+     let hour    = now.getHours();
+     let minute  = now.getMinutes();
+     let second = now.getSeconds(); 
+    
+         if(month.toString().length === 1) {
+             month = '0' + month;
+         }
+         if(day.toString().length === 1) {
+             day = '0' + day;
+         }   
+         if(hour.toString().length === 1) {
+             hour = '0' + hour;
+         }
+         if(minute.toString().length === 1) {
+             minute = '0' + minute;
+         }
+         if(second.toString().length === 1) {
+             second = '0' + second;
+         }   
+     return `${year}-${month}-${day} ${hour}:${minute}:${second}`;   
+}
+ // example usage: realtime clock
+
+ setInterval(function() {
+     currentTime = formatTime();
+     document.querySelector('date').innerHTML = currentTime;
+}, 1000);*/
+
+function isValidText(text) {
+  if (
+    typeof text !== "string" ||
+    text.trim() === "" ||
+    text[0].toUpperCase() !== text[0]
+  ) {
+    return false;
+  }
+
+  return true;
 }
 
 // CRUD operations:
